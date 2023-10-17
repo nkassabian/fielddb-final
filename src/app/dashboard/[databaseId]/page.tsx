3d14f93cd6dc4b290ff97cb4dee3e32bddac5b83
Noubar Kassabian <noubark16@gmail.com>
2023-10-17 23:52:03 +0300

    Update page.tsx

--- src/app/dashboard/[databaseId]/page.tsx
+++ src/app/dashboard/[databaseId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect } from "react";
+import React, { ReactNode, useCallback, useEffect } from "react";
 import ReactFlow, {
   useNodesState,
   useEdgesState,
@@ -8,6 +8,7 @@ import ReactFlow, {
   Controls,
   MiniMap,
   Background,
+  NodeMouseHandler,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
@@ -32,35 +33,46 @@ interface PageProps {
   };
 }
 const Page = ({ params }: PageProps) => {
-  const [drawerOpened, setDrawerOpened] = useState<boolean>(true);
+  const [drawerOpened, setDrawerOpened] = useState<boolean>(false);
+
+  const [selectedNode, setSelectednNode] = useState<ReactNode>(null);
 
   const initialNodes = [
     { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
     { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
   ];
+  const onNodeDoubleClick = (event: React.MouseEvent, node: ReactNode) => {
+    console.log(JSON.stringify(selectedNode));
+    console.log(JSON.stringify(node));
+    if (JSON.stringify(selectedNode) != JSON.stringify(node)) {
+      console.log("YESS");
+      setDrawerOpened(false);
+      setTimeout(() => {
+        setSelectednNode(node);
+        setDrawerOpened(true);
+      }, 100);
+    } else {
+      setDrawerOpened(!drawerOpened);
+    }
+  };
+
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex">
         {/* Left sidebar & main wrapper */}
         <div className="w-24 px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6 ">
-          <Button
-            onClick={() => {
-              setDrawerOpened(!drawerOpened);
-            }}
-          >
-            Open
-          </Button>
           <div style={{ width: "100%", height: "100%" }}>
-            <ReactFlow nodes={initialNodes}>
+            <ReactFlow nodes={initialNodes} onNodeClick={onNodeDoubleClick}>
               <Controls />
               <MiniMap />
               <Background />
             </ReactFlow>
           </div>
         </div>
+
         <div
           className={cn(
-            " border-l border-zinc-300 transition-all duration-200 ease-in-out shadow-lg",
+            " border-l border-zinc-300 transition-all duration-100 ease-in-out shadow-lg",
             drawerOpened == true ? "w-[25%]" : "w-0"
           )}
         >
